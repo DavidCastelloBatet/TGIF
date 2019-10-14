0b89bedb4e96926689898d332f07b351b4e6b511
David Castello <davidcastellobatetgmail.com>
2019-10-14 10:17:31 +0200

    1410 unifico js a main.js

---
+++ JS/main.js
@@ -0,0 +1,51 @@
+// Variables globales
+var members = data.results[0].members;
+
+// Llamadas a funciones
+addTable(members);
+
+// Declaración de funciones
+function addTable(members) {
+  var table = document.getElementById("t-body");
+
+  for (var i = 0, len = members.length; i < len; i++) {
+    var firstName = members[i].first_name;
+    var lastName = members[i].last_name;
+    var midName = members[i].middle_name;
+    if (midName == null) {
+      midName = "";
+    }
+    var fullName = firstName + " " + midName + " " + lastName;
+    var urlName = members[i].url;
+
+    var row = document.createElement("tr");
+    var ancor = document.createElement("a");
+    ancor.setAttribute("href", urlName);
+    ancor.setAttribute("target", "_blank");
+    ancor.innerHTML = fullName;
+
+    var nameCell = document.createElement("td");
+    var partyCell = document.createElement("td");
+    var stateCell = document.createElement("td");
+    var seniorCell = document.createElement("td");
+    var votesCell = document.createElement("td");
+
+    nameCell.append(ancor);
+
+    var party = members[i].party;
+    partyCell.append(party);
+
+    var state = members[i].state;
+    stateCell.append(state);
+
+    var seniority = members[i].seniority;
+    seniorCell.append(seniority);
+
+    var votes = members[i].votes_with_party_pct;
+    votesCell.append(votes);
+
+    row.append(nameCell, partyCell, stateCell, seniorCell, votesCell);
+
+    table.append(row);
+  }
+}
