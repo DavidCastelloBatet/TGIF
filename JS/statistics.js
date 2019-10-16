2fa25852d5e4cc7bc23c311c5e537848e0ac6857
David Castello <davidcastellobatetgmail.com>
2019-10-16 08:32:17 +0200

    1510 feina nit

---
+++ JS/statistics.js
@@ -0,0 +1,16 @@
+var estadisticas = {    
+    "totalD":0,
+    "totalR":0,
+    "totalI":0,
+    "totales":0,
+    "totPorcentD":0,
+    "totPorcentR":0,
+    "totPorcentI":0,
+    "totalPorcent":0,
+    "least_engange":[],
+    "most_engange":[],
+    "least_loyal":[],
+    "most_loyal":[],
+    "totalStates":[]
+    }
+    
